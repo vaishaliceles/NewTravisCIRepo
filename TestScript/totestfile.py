2e7099ee0aa8217286784a91649ffac03615a0a6
vaishaliceles <84778498+vaishaliceles@users.noreply.github.com>
2021-11-18 13:16:42 +0530

    Update totestfile.py

--- TestScript/totestfile.py
+++ TestScript/totestfile.py
@@ -3,6 +3,7 @@ import random
 
 print(f"Printing a randome number{random.random()}")
 print("Lets test")
+print(f"Printing a randome number{random.random()}")
 print("one more user added")
 print("fourth user")
 print("to check amount of credit debited")
