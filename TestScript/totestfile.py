5c4c3701dbe3099a852333d913235553e852e96d
Vaishali Singh <vaishali.s@celestialsys.com>
2021-11-18 13:30:23 +0530

    adding new branch

--- TestScript/totestfile.py
+++ TestScript/totestfile.py
@@ -2,6 +2,7 @@ import random
 
 
 print(f"Printing a randome number{random.random()}")
+print(f"Printing a randome number{random.random()}")
 print("Lets test")
 print(f"Printing a randome number{random.random()}")
 print("one more user added")
